3f7ff0ba378c4c9a41610dad3eb45d44e6fe414f
JayOMk <puh2794@gmail.com>
2024-02-20 16:26:35 +0900

    Start

---
+++ myProject_lib/src/main/java/com/lib/MyProjectLibApplication.java
@@ -0,0 +1,13 @@
+package com.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectLibApplication.class, args);
+	}
+
+}
